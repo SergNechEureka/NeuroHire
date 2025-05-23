abbf0211244f67a55e36f4692aac0d75cdccf530
Sergej A Nechaev <sergei.nechaev@eureka-it.de>
2025-05-23 09:22:59 +0200

    DB Connection  has been moved into separate file

--- backend/file_metadata/db.py
+++ backend/file_metadata/db.py
@@ -1,6 +1,12 @@
 from sqlmodel import create_engine, Session
+from pathlib import Path
 
-DATABASE_URL = "postgresql://neuroadmin:password@localhost/neurohire"
+def load_connection(filename: str) -> str:
+    prompt_path = Path(__file__).parent.parent / "DBConnections" / filename
+    with open(prompt_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+DATABASE_URL = load_connection("PostgreSQL.txt")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
