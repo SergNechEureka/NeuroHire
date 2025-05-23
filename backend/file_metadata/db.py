198fa97da6236b5d679288f5301c404e09cd11e9
Sergej A Nechaev <sergei.nechaev@eureka-it.de>
2025-05-23 11:10:40 +0200

    DB Connection per environment variable

--- backend/file_metadata/db.py
+++ backend/file_metadata/db.py
@@ -1,13 +1,10 @@
+import os
 from sqlmodel import create_engine, Session
-from pathlib import Path
+from dotenv import load_dotenv
 
-def load_connection(filename: str) -> str:
-    prompt_path = Path(__file__).parent.parent / "DBConnections" / filename
-    with open(prompt_path, "r", encoding="utf-8") as f:
-        return f.read()
+load_dotenv()
 
-DATABASE_URL = load_connection("PostgreSQL.txt")
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
 
 def get_session():
     with Session(engine) as session:
