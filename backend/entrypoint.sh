87e27809e4e81a12cf22f272eb81d0ef861187f6
Sergej A Nechaev <sergei.nechaev@eureka-it.de>
2025-06-11 23:28:01 +0200

    Start per Entrypoint 3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-if [ "$MODE" = "worker" ]; then
+if [ "$MODE" = "SCANNER" ]; then
     exec python worker.py
-else
+elif [ "$MODE" = "API" ]; then
     exec uvicorn main:app --host 0.0.0.0 --port 5050
+else
+    echo "Unknown MODE: $MODE. Please set MODE to either 'SCANNER' or 'API'."
 fi
